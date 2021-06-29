d2682da8d83edd78864a686f101abe9819ebdfa8
Agnes <agnes.tshibangugrillon@gmail.com>
2021-06-29 03:34:09 +0200

    animation ok git add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -A YOUHOUUUUUUUUUUUUUUUUUUUUUU

---
+++ app.js
@@ -0,0 +1,14 @@
+let planet = document.getElementById('planet')
+let wing =  document.getElementById('wing')
+let cloud1 =  document.getElementById('cloud1')
+
+window.addEventListener('srcoll', function() {
+    var valueY = window.scrollY;
+
+    planet.style.left= valueY * 300 + 'px';
+    console.log(hi);
+
+
+
+
+})
